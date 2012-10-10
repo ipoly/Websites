963fe23b99429b7ed2ad9cc8fbf569add139dbe0
ipoly <89490364a@gmail.com>
2012-10-10 11:18:30 +0800

    修正lte-ie7的bug

--- src/js/append.js
+++ src/js/append.js
@@ -1,5 +1,5 @@
 // 页脚加载的js
 //@codekit-prepend "jquery.easing.min.js";
+//@codekit-prepend "jquery-ui-1.9.0.custom.min.js";
 //@codekit-prepend "juicer.js";
 //@codekit-prepend "lte-ie7.js";
-//@codekit-prepend "jquery-ui-1.9.0.custom.min.js";
